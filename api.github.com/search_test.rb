98365121c0e12661062e7f39da81a3f6d2983a80
smsohan <sohan39@gmail.com>
2014-12-30 16:40:45 -0700

    finally specs passing

--- api.github.com/search_test.rb
+++ api.github.com/search_test.rb
@@ -7,8 +7,9 @@ require_relative 'github'
 describe "Github/search" do
   describe 'search for user' do
     it "searches for a user by name" do
-      response = Github.get '/users/smsohan/repos', verify: false
-      # assert_equals response.response.body, 200
+      response = Github.get('/users/smsohan/repos', verify: false)
+      assert_equal 200, response.code
+      refute_nil response.body
     end
   end
 end
