dd46cf7f7942f3281cdca9e527872337413de6f2
smsohan <sohan39@gmail.com>
2014-12-30 16:46:39 -0700

    adds the header

--- api.github.com/search_test.rb
+++ api.github.com/search_test.rb
@@ -5,11 +5,19 @@ require "minitest/autorun"
 require_relative 'github'
 
 describe "Github/search" do
-  describe 'search for user' do
-    it "searches for a user by name" do
-      response = Github.get('/users/smsohan/repos', verify: false)
+
+  describe 'Search repositories' do
+
+    it 'Suppose you want to search for popular Tetris repositories written in Assembly. Your query might look like this.' do
+      response = Github.get '/search/repositories?q=tetris+language:assembly&sort=stars&order=desc',
+        verify: false,
+        headers: {
+          'x-spy-rest-desc' =>'Suppose you want to search for popular Tetris repositories written in Assembly. Your query might look like this.',
+          'x-spy-rest-resource' => 'search'
+        }
       assert_equal 200, response.code
       refute_nil response.body
     end
   end
+
 end
