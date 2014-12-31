d551c522551a6f4ce077e54275043e56872635ce
smsohan <sohan39@gmail.com>
2014-12-31 15:01:29 -0700

    addes a test for gists

---
+++ api.github.com/gists_test.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require "minitest/autorun"
+require_relative 'github'
+
+describe 'gists' do
+  before do
+    description = (self.name =~ /^test_\d+_(.*)$/) && $1
+    headers = {
+      'x-spy-rest-desc' => description,
+      'x-spy-rest-resource' => self.class.name.split("::").first
+    }
+    @common_options = {
+      verify: false,
+      headers: headers
+    }
+  end
+
+  it "List a user's gists since a time:" do
+    response = Github.get '/users/smsohan/gists?since=20140101T00:00:00Z', @common_options
+    assert_equal 200, response.code
+    refute_nil response.body
+  end
+
+end
+
--- api.github.com/github.rb
+++ api.github.com/github.rb
@@ -6,7 +6,7 @@ class Github
   include HTTParty
 
   base_uri 'https://api.github.com'
-  http_proxy 'spyrest.com', 9081
+  http_proxy 'localhost', 9080
   follow_redirects true
   headers('Accept' => 'application/vnd.github.v3+json', 'User-Agent' => 'curl/7.37.1')
 end
