2fb2b926dcd5a59c008bfa9a5212dd228b8c295e
smsohan <sohan39@gmail.com>
2014-12-30 17:01:35 -0700

    adds a 2nd test for search

--- api.github.com/search_test.rb
+++ api.github.com/search_test.rb
@@ -4,20 +4,31 @@ require 'bundler/setup'
 require "minitest/autorun"
 require_relative 'github'
 
-describe "Github/search" do
+describe "search" do
 
-  describe 'Search repositories' do
+  before do
+    description = (self.name =~ /^test_\d+_(.*)$/) && $1
+    headers = {
+      'x-spy-rest-desc' => description,
+      'x-spy-rest-resource' => self.class.name.split("::").first
+    }
+    @common_options = {
+      verify: false,
+      headers: headers
+    }
+  end
 
+  describe 'repositories' do
     it 'Suppose you want to search for popular Tetris repositories written in Assembly. Your query might look like this.' do
+      response = Github.get '/search/repositories?q=tetris+language:assembly&sort=stars&order=desc', @common_options
+      assert_equal 200, response.code
+      refute_nil response.body
+    end
+  end
 
-      puts "self = #{self.inspect}"
-
-      response = Github.get '/search/repositories?q=tetris+language:assembly&sort=stars&order=desc',
-        verify: false,
-        headers: {
-          'x-spy-rest-desc' =>'Suppose you want to search for popular Tetris repositories written in Assembly. Your query might look like this.',
-          'x-spy-rest-resource' => 'search'
-        }
+  describe 'Search code' do
+    it 'Suppose you want to find the definition of the addClass function inside jQuery. Your query would look something like this:' do
+      response = Github.get '/search/code?q=addClass+in:file+language:js+repo:jquery/jquery', @common_options
       assert_equal 200, response.code
       refute_nil response.body
     end
